2479abeaa8601ee50dc99a468aa0209e3badc69b
下雨天玩游戏 <17826800084g@gmail.com>
2022-02-21 18:05:53 +0800

    add function: clear_all_interval

---
+++ tampermonkey.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://stackoverflow.com/questions/8635502/how-do-i-clear-all-intervals
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackoverflow.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    clear_all_interval = function() {
+        // Get a reference to the last interval + 1
+        const interval_id = window.setInterval(function(){}, Number.MAX_SAFE_INTEGER);
+
+        // Clear any timeout/interval up to that id
+        for (let i = 1; i < interval_id; i++) {
+            window.clearInterval(i);
+        }
+    }
+})();
